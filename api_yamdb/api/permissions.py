f7a2d195a1aaff888d2e98c17552edbf520f8aab
unknown <daniil.tsiganov@gmail.com>
2022-06-28 22:21:04 +0300

    flake8 urls.py test

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,11 +3,9 @@ from rest_framework import permissions
 
 class AdminOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (
-            request.user.is_admin
+        return (request.user.is_admin
             or request.user.is_staff
-            or request.user.is_superuser
-        )
+            or request.user.is_superuser)
 
 
 class AdminOrReadOnly(permissions.BasePermission):
@@ -22,11 +20,9 @@ class AdminOrReadOnly(permissions.BasePermission):
 
 class IsAuthorOrHasRightsOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if (
-            request.method in permissions.SAFE_METHODS
+        if (request.method in permissions.SAFE_METHODS
             or request.user == obj.author
             or request.user.is_admin
             or request.user.is_moderator
-            or request.user.is_staff
-        ):
+            or request.user.is_staff):
             return True
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,19 +2,17 @@ from django.urls import include, path
 from rest_framework import routers
 
 from .views import (APIGetToken,APISignup,
-    CategoriesViewSet, CommentsViewSet,
-    GenresViewSet, ReviewViewSet,
-    TitlesViewSet, UsersViewSet)
+                    CategoriesViewSet, CommentsViewSet,
+                    GenresViewSet, ReviewViewSet,
+                    TitlesViewSet, UsersViewSet)
 
 router_v1 = routers.DefaultRouter()
 router_v1.register(
-    r"titles/(?P<title_id>\d+)/reviews", ReviewViewSet, basename="reviews"
-)
+    r"titles/(?P<title_id>\d+)/reviews", ReviewViewSet, basename="reviews")
 router_v1.register(
     r"titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments",
     CommentsViewSet,
-    basename="comments",
-)
+    basename="comments")
 router_v1.register("genres", GenresViewSet)
 router_v1.register("titles", TitlesViewSet)
 router_v1.register("categories", CategoriesViewSet)
